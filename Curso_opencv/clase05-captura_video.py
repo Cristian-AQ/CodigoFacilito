832fae8441d7a028432be91e065eead90edb99f5
Cristian-Aquino <yojar.aquino@unmsm.edu.pe>
2021-04-14 17:40:58 -0500

    ligero_cambio

--- Curso_opencv/clase05-captura_video.py
+++ Curso_opencv/clase05-captura_video.py
@@ -7,7 +7,7 @@ salida = cv2.VideoWriter('video\VideoPrueba.avi',cv2.VideoWriter_fourcc(*'XVID')
 
 while (video.isOpened()):#lee la imagen a cada momento
     ret,imagen = video.read()
-    if ret is True:
+    if ret == True:
         cv2.imshow('Video',imagen)
         salida.write(imagen)
         if cv2.waitKey(500) & 0xFF == ord('x'):#0xff si se trabaja en maquina de 65 bits
